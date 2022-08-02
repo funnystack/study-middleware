6fd1e39998356d588a358d034a4ad6fa1fa6d6ba
方利 <fangli@autohome.com.cn>
2022-08-02 18:57:44 +0800

    kafka的生产和消费

--- study-kafka/src/main/java/com/funny/study/kafka/config/KafkaConfig.java
+++ study-kafka/src/main/java/com/funny/study/kafka/config/KafkaConfig.java
@@ -5,7 +5,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -14,7 +13,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableKafka
 public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
@@ -26,7 +24,7 @@ public class KafkaConfig {
     @Value("${spring.kafka.producer.buffer-memory}")
     private int bufferMemory;
 
-    public Map<String, Object> travelCpmpareProducerConfigs() {
+    public Map<String, Object> kafkaConfigs() {
         return getStringObjectMap(bootstrapServers, retries, batchSize, bufferMemory);
     }
 
@@ -41,13 +39,13 @@ public class KafkaConfig {
         return props;
     }
 
-    public ProducerFactory<String, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(travelCpmpareProducerConfigs());
+    public ProducerFactory<String, String> kafkaFactory() {
+        return new DefaultKafkaProducerFactory<>(kafkaConfigs());
     }
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        return new KafkaTemplate<>(kafkaFactory());
     }
 
 
